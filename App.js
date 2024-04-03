10b81edf97fa87292fee638acdc16582f0bfeec4
pagott0 <enzovaspagotto@gmail.com>
2024-04-03 01:12:49 -0300

    feat: loginPage

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
-import { Button, StyleSheet, Text, View } from 'react-native';
+import { Button, Linking, Pressable, StyleSheet, Text, View } from 'react-native';
 import LoginPage from './src/app/login/LoginPage.tsx';
+import RegisterPage from './src/app/register/RegisterPage.tsx';
 
 export default function App() {
   return (
--- src/app/register/RegisterPage.tsx
+++ src/app/register/RegisterPage.tsx
@@ -1,23 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
-import { useState } from 'react';
 import { Button, Linking, Pressable, StyleSheet, Text, TextInput, TextInputBase, TextInputComponent, TouchableHighlight, TouchableOpacity, View } from 'react-native';
 
-
-export default function LoginPage() {
-  
-  const [emailValue, setEmailValue] = useState('');
-  const [passwordValue, setPasswordValue] = useState('');
-
+export default function RegisterPage() {
   return (
     <View className="flex-1 items-center justify-center bg-white gap-y-2 bg-zinc-50">
       <Text className='font-bold text-5xl text-green-900 mb-4'>Petty!</Text>
       <View className='w-full items-center flex flex-col gap-2'>
         <Text className='w-5/6 font-bold text-lg'>E-mail</Text>
-        <TextInput placeholder='-' className='bg-white border border-green-500 w-5/6 p-5 border rounded-md' onChangeText={setEmailValue} value={emailValue} inputMode='email'/>
+        <TextInput placeholder='test' className='bg-white border border-green-500 w-5/6 p-5 border rounded-md' onChange={(e) => alert()}/>
       </View>
       <View className='mb-2 w-full items-center flex flex-col gap-2'>
         <Text className='w-5/6 font-bold text-lg'>Senha</Text>
-        <TextInput placeholder='-' className='bg-white border border-green-500 w-5/6 p-5 border rounded-md' secureTextEntry={true} onChangeText={setPasswordValue} inputMode='text' value={passwordValue}/>
+        <TextInput placeholder='test' className='bg-white border border-green-500 w-5/6 p-5 border rounded-md' onChange={(e) => alert()} inputMode='text'/>
+      </View>
+      <View className='mb-2 w-full items-center flex flex-col gap-2'>
+        <Text className='w-5/6 font-bold text-lg'>Confirmar Senha</Text>
+        <TextInput placeholder='test' className='bg-white border border-green-500 w-5/6 p-5 border rounded-md' onChange={(e) => alert()} inputMode='text'/>
+      </View>
+      <View className='mb-2 w-full items-center flex flex-col gap-2'>
+        <Text className='w-5/6 font-bold text-lg'>Cidade</Text>
+        <TextInput placeholder='test' className='bg-white border border-green-500 w-5/6 p-5 border rounded-md' onChange={(e) => alert()} inputMode='text'/>
       </View>
       <Pressable className='mb-2 bg-green-950 p-5 w-5/6 items-center rounded-xl transform hover:scale-110 active:bg-emerald-800' onPress={() => alert('test')}>
         <Text className='font-bold text-lg text-white'>Entrar</Text>
